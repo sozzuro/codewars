3510b6c96f1ab1a645507b4275c7572fbfdad23c
Denis Radivil <sozzuro@gmail.com>
2022-02-02 22:04:46 +0300

    feat: completed Convert to Binary task

---
+++ 3.js
@@ -0,0 +1,10 @@
+/*
+Convert to Binary
+
+Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+
+*/
+
+function toBinary(n){
+    return +parseInt(n).toString(2);
+  }
